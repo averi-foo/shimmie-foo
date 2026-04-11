4cb1e8fef1d46b3784bd670615cc85172c8f7135
s5lachlan <s5s5s5wum@gmail.com>
2026-04-11 04:00:01 +0100

    Edit user theme thing

--- themes/averi/user.theme.php
+++ themes/averi/user.theme.php
@@ -15,9 +15,6 @@ class AveriUserPageTheme extends UserPageTheme
 		Ctx::$page->set_title("Login"); // Set page title
 		$this->display_navigation(); // Display navbar on left
 		$html = $this->create_login_block();
-		if (Ctx::$config->get(UserAccountsConfig::SIGNUP_ENABLED)) {
-			$html->appendChild(SMALL(A(["href" => make_link("user_admin/create")], "Create Account")));
-		}
 		Ctx::$page->add_block(new Block("Login", $html, "main", 90));
 	}
 	
@@ -31,6 +28,42 @@ class AveriUserPageTheme extends UserPageTheme
 	public function display_login_block(): void
 	{
 		// no block in this theme
+	}
+	
+	public function create_login_block(): HTMLElement
+	{
+		$captcha = Captcha::get_html(UserAccountsPermission::SKIP_LOGIN_CAPTCHA);
+		
+		$form = SHM_SIMPLE_FORM(
+			make_link("user_admin/login"),
+			TABLE(["style" => "width: 50%", "class" => "form"],
+			TBODY(
+			TR(
+				TH(LABEL(["for" => "user"], "Name")),
+				TD(INPUT(["id" => "user", "type" => "text", "name" => "user", "autocomplete" => "username", "required" => true]))
+			),
+		   TR(
+				TH(LABEL(["for" => "pass"], "Password")),
+				TD(INPUT(["id" => "pass", "type" => "password", "name" => "pass", "autocomplete" => "current-password", "required" => true]))
+		   ),
+		   $captcha ? TR(
+			   TH(LABEL(["for" => "captcha"], "Captcha")),
+				TD($captcha)
+		   ) : null
+			),
+			  TFOOT(
+				  TR(TD(["colspan" => "2"], INPUT(["type" => "submit", "value" => "Log In"])))
+			  )
+			)
+		);
+		
+		$html = emptyHTML();
+		$html->appendChild($form);
+		if (Ctx::$config->get(UserAccountsConfig::SIGNUP_ENABLED) && Ctx::$user->can(UserAccountsPermission::CREATE_USER)) {
+			$html->appendChild(SMALL(A(["href" => make_link("user_admin/create")], "Create Account")));
+		}
+		
+		return $html;
 	}
 	
 	/**
