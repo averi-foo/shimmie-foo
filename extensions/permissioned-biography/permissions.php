a226a6d199a152d10e9980f54be9aa1423f0bb3a
s5lachlan <s5s5s5wum@gmail.com>
2026-04-11 23:34:07 +0100

    Well for starters the new permission is not showing up

--- extensions/permissioned-biography/permissions.php
+++ extensions/permissioned-biography/permissions.php
@@ -7,6 +7,7 @@ namespace Shimmie2;
 final class UserBiosPermissionsPermission extends PermissionGroup
 {
 	public const KEY = "edit_own_bio";
+	public ?string $title = "Edit own bio";
 	 
 	#[PermissionMeta("Edit own bio")]
 	public const EDIT_OWN_BIO = "edit_own_bio";
